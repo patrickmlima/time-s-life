ea516e73f598757a78ff4580d02af5e081c663e2
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-04 22:54:22 -0400

    .

--- TimesLife/src/model/teste/TesteConsulta.java
+++ TimesLife/src/model/teste/TesteConsulta.java
@@ -1,14 +1,31 @@
 package model.teste;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import model.person.Monitorado;
+import model.person.Pessoa;
 import model.teste.util.JPAUtil;
 
 public class TesteConsulta {
-	EntityManager em = new JPAUtil().getEntityManager();
+	public static void main(String[] args) {
+		EntityManager em = new JPAUtil().getEntityManager();
+		
+//		Query q2 = em.createQuery("Select d from Dado d where d.")
+		
+//		Query q1 = em.createQuery("Select p from Pessoa p where p.identificador =:vId ");
+//		q1.setParameter("vId", 1);
 	
-	Query q1 = em.createQuery("Select p from Pessoa p where p.identificador =:vId and p.tipo =:vtipo oderd by p.identificador");
-	
+//		
+//		
+//		List<Pessoa> pessoas = q1.getResultList();
+//		
+//		for(Pessoa p : pessoas) {
+//			System.out.println(p.getNome() + " - " + p.getDataNasc() + " ~~ " + ((Monitorado)p).getBATIMENTO_IDEAL_MINIMO()
+//					+ " / " + ((Monitorado)p).getBATIMENTO_IDEAL_MAXIMO());
+//		}
+	}
 	
 }
