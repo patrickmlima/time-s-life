301cc78e4d0fa1f043d1a09b25741d5d84881e1a
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-04 22:14:30 -0400

    Interface para PessoaDAO

--- TimesLife/src/model/dao/PessoaDAO.java
+++ TimesLife/src/model/dao/PessoaDAO.java
@@ -5,7 +5,7 @@ import model.person.Pessoa;
 public interface PessoaDAO {
 	
 	public void adicionar(Pessoa pessoa);
-	public void consultar(Pessoa pessoa);
+	public Pessoa consultar(Integer identificador);
 	public void editar(Pessoa pessoa);
 	public void excluir(Pessoa pessoa);
 	public void listar();
