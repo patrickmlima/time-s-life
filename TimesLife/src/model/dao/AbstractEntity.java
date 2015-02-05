2390fa6e323171addceab93b024fa1f99146bbe8
Pedro Augusto <pedroribeiro212@gmail.com>
2015-02-05 18:06:06 -0200

    entidade geneérica

---
+++ TimesLife/src/model/dao/AbstractEntity.java
@@ -0,0 +1,41 @@
+package model.dao;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+public abstract class AbstractEntity<T> implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) 
+			return true;
+		if(obj == null) 
+			return false;
+		if(getClass() != obj.getClass())
+			return false;
+		AbstractEntity<T> other = (AbstractEntity<T>) obj;
+		if(getId() == null) {
+			if(other.getId() != null)
+				return false;
+		} else if(!getId().equals(other.getId()))
+			return false;
+		return true;
+	}
+	
+	public abstract T getId();
+	
+	
+}
