3eff40c81cb9bbeae91b2ee9ffa7aed91c53793e
Pedro Augusto <pedroribeiro212@gmail.com>
2015-02-04 23:51:15 -0200

    interface DadosDAO definida

--- TimesLife/src/model/dao/DadosDAO.java
+++ TimesLife/src/model/dao/DadosDAO.java
@@ -1,9 +1,11 @@
 package model.dao;
 
+import java.util.List;
+
 import model.person.Dado;
 
 public interface DadosDAO {
 	
 	public void adicionar(Dado dado);
-	public void consultar();
+	public List<Dado> consultar(Integer monitoradoId);
 }
