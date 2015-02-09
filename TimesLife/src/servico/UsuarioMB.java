90ca65a8580301394a3972ac77eb38eae3ad478c
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-09 12:48:30 -0400

    UsuarioMB

--- TimesLife/src/servico/UsuarioMB.java
+++ TimesLife/src/servico/UsuarioMB.java
@@ -4,13 +4,39 @@ import javax.faces.bean.ManagedBean;
 
 import model.dao.PessoaDAO;
 import model.person.Pessoa;
+import model.person.TipoUsuario;
 import model.person.Usuario;
 
 @ManagedBean
 public class UsuarioMB {
 	Usuario usuario = new Usuario();
+	TipoUsuario tipo;
 	PessoaDAO dao = new PessoaDAO();
 	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public TipoUsuario getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoUsuario tipo) {
+		this.tipo = tipo;
+	}
+
+	public PessoaDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(PessoaDAO dao) {
+		this.dao = dao;
+	}
+
 	public void salvar() {
 		dao.adicionar(usuario);
 	}
