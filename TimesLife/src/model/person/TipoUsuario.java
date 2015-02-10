2e791c085913bc84cf1d377f572788a3487ba44a
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-10 13:15:30 -0400

    melhoria no enum de tipo de usuário

--- TimesLife/src/model/person/TipoUsuario.java
+++ TimesLife/src/model/person/TipoUsuario.java
@@ -1,5 +1,17 @@
 package model.person;
 
 public enum TipoUsuario {
-	ADMIN, MONITOR;
+	ADMIN("Administrador"), 
+	MONITOR("Monitor");
+	
+	private String label;
+	
+	private TipoUsuario(String label) {
+		this.label = label;
+	}
+	
+	public String getLabel() {
+		return this.label;
+	}
+	 
 }
