6328a733732dee08b6899da8ffcae82f66d617dd
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-11 00:06:23 -0400

    DAO de usuario

---
+++ TimesLife/src/model/dao/UsuarioDao.java
@@ -0,0 +1,24 @@
+package model.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import model.person.TipoUsuario;
+import model.person.Usuario;
+import model.teste.util.JPAUtil;
+
+public class UsuarioDao extends AbstractDao<Usuario>{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public List<Usuario> listarMonitores() {
+		EntityManager em = new JPAUtil().getEntityManager();
+		Query q = em.createQuery("Select u from Usuario u where u.tipo=:vTipo");
+		q.setParameter("vTipo", TipoUsuario.MONITOR);
+		return q.getResultList();
+	}
+}
