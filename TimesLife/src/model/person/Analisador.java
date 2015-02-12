87f9b4c56d001dfd18ecdac4629932a9b2df95ee
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-12 13:08:27 -0400

    modelo do analisador (ainda não implementado)

--- TimesLife/src/model/person/Analisador.java
+++ TimesLife/src/model/person/Analisador.java
@@ -1,13 +1,21 @@
 package model.person;
 
+import model.strategy.avisar.Alertavel;
+import model.strategy.avisar.AvisarNaTela;
+
 public class Analisador {
+	private Alertavel alert;
 	
 	public void classificar(Monitorado monitorado, Dado dado) {
-		
+		if(dado.getBpm() > monitorado.getBATIMENTO_IDEAL_MAXIMO()) {
+			//batimentos altos
+		} else if(dado.getBpm() < monitorado.getBATIMENTO_IDEAL_MINIMO()) {
+			//batimentos baixos
+		}
 	}
 	
-	public void avisar(Monitorado monitorado) {
-		
+	public void avisar() {
+		 alert = new AvisarNaTela();
+		 //chamar avisar
 	}
-
 }
