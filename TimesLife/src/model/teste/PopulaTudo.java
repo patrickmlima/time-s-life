5ef7b917e438eef4a8f2a5362ecf7f6d795cf9a0
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-19 21:20:37 -0400

    teste para popular o banco de dados

--- TimesLife/src/model/teste/PopulaTudo.java
+++ TimesLife/src/model/teste/PopulaTudo.java
@@ -10,45 +10,45 @@ import model.person.Usuario;
 
 public class PopulaTudo {
 	public static void main(String[] args) {
-		MonitoradoDao mDao = new MonitoradoDao();
-		UsuarioDao uDao = new UsuarioDao();
-		
-		Monitorado mon1 = new Monitorado();
-		mon1.setNome("monitorado teste1");
-		mon1.setEndereco("rua 1");
-		Calendar c = Calendar.getInstance();
-		c.set(1998, 12, 21);
-		mon1.setDataNasc(c);
-		mon1.setAltura(1.60f);
-		mon1.setPeso(60.5f);
-		mon1.setTelefone("991919191");
-		mon1.setBATIMENTO_IDAL_MAXIMO(200);
-		mon1.setBATIMENTO_IDEAL_MINIMO(70);
-		
-		Monitorado mon2 = new Monitorado();
-		mon2.setNome("monitorado teste2");
-		mon2.setEndereco("rua 2");
-		c.set(1996, 12, 22);
-		mon2.setDataNasc(c);
-		mon2.setAltura(1.70f);
-		mon2.setPeso(65.0f);
-		mon2.setTelefone("992929292");
-		mon2.setBATIMENTO_IDAL_MAXIMO(210);
-		mon2.setBATIMENTO_IDEAL_MINIMO(90);
-		
-		Usuario user1 = new Usuario();
-		user1.setNome("usuario teste1");
-		user1.setEndereco("rua 01");
-		c.set(1911, 11, 11);
-		user1.setDataNasc(c);
-		user1.setTelefone("992939192");
-		user1.setTipo(TipoUsuario.MONITOR);
-		user1.setMonitorados(mon1);
-		user1.setMonitorados(mon2);
-		
-		mDao.adicionar(mon1);
-		mDao.adicionar(mon2);
-		uDao.adicionar(user1);
+//		MonitoradoDao mDao = new MonitoradoDao();
+//		UsuarioDao uDao = new UsuarioDao();
+//		
+//		Monitorado mon1 = new Monitorado();
+//		mon1.setNome("monitorado teste1");
+//		mon1.setEndereco("rua 1");
+//		Calendar c = Calendar.getInstance();
+//		c.set(1998, 12, 21);
+//		mon1.setDataNasc(c);
+//		mon1.setAltura(1.60f);
+//		mon1.setPeso(60.5f);
+//		mon1.setTelefone("991919191");
+//		mon1.setBATIMENTO_IDAL_MAXIMO(200);
+//		mon1.setBATIMENTO_IDEAL_MINIMO(70);
+//		
+//		Monitorado mon2 = new Monitorado();
+//		mon2.setNome("monitorado teste2");
+//		mon2.setEndereco("rua 2");
+//		c.set(1996, 12, 22);
+//		mon2.setDataNasc(c);
+//		mon2.setAltura(1.70f);
+//		mon2.setPeso(65.0f);
+//		mon2.setTelefone("992929292");
+//		mon2.setBATIMENTO_IDAL_MAXIMO(210);
+//		mon2.setBATIMENTO_IDEAL_MINIMO(90);
+//		
+//		Usuario user1 = new Usuario();
+//		user1.setNome("usuario teste1");
+//		user1.setEndereco("rua 01");
+//		c.set(1911, 11, 11);
+//		user1.setDataNasc(c);
+//		user1.setTelefone("992939192");
+//		user1.setTipo(TipoUsuario.MONITOR);
+//		user1.setMonitorados(mon1);
+//		user1.setMonitorados(mon2);
+//		
+//		mDao.adicionar(mon1);
+//		mDao.adicionar(mon2);
+//		uDao.adicionar(user1);
 	}
 
 }
