90982b10c1bb7c8ea292b172c585d1532d193a0c
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-21 01:41:29 -0400

    managed bean para cadastro de usuários

--- TimesLife/src/servico/CadastrarUsuarioMB.java
+++ TimesLife/src/servico/CadastrarUsuarioMB.java
@@ -1,29 +1,25 @@
 package servico;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
-import model.dao.UsuarioDao;
-import model.person.Pessoa;
 import model.person.TipoUsuario;
 import model.person.Usuario;
 
 @ManagedBean
-@RequestScoped
-public class UsuarioMB {
+@ViewScoped
+public class CadastrarUsuarioMB {
 	private Usuario usuario = new Usuario();
 	private Date dataNasc;
 	private TipoUsuario tipo;
 	
-	private UsuarioDao dao = new UsuarioDao();
-	private List<Usuario> monitoresList;
-	private Usuario monitorSelecionado;
+	private ServicoDAO dao = new ServicoDAO();
+
 	
-	public UsuarioMB() {
-		monitoresList = dao.listarMonitores();
+	public CadastrarUsuarioMB() {
+		
 	}
 
 	public Usuario getUsuario() {
@@ -50,57 +46,20 @@ public class UsuarioMB {
 	public void setTipo(TipoUsuario tipo) {
 		this.tipo = tipo;
 	}
-
-	public UsuarioDao getDao() {
-		return dao;
-	}
-
-	public void setDao(UsuarioDao dao) {
-		this.dao = dao;
-	}
-	
-	public List<Usuario> getMonitoresList() {
-		return monitoresList;
-	}
-	
-	public void setMonitoresList(List<Usuario> monitoresList) {
-		this.monitoresList = monitoresList;
-	}
-	
-	public Usuario getMonitorSelecionado() {
-		return monitorSelecionado;
-	}
-	
-	public void setmonitorSelecionado(Usuario monitorSelecionado) {
-		this.monitorSelecionado = monitorSelecionado;
-	}
 	
 
 	public void salvar() {
 		try {
 		usuario.setDataNasc(dataNasc);
-		dao.adicionar(usuario);
-		DialogMB.showMessage("Parabéns", "Usuário adicionado com sucesso!");
+		dao.salvarUsuario(usuario);
+		DialogMB.showMessage("Parabéns", "Usuário adicionado com sucesso!", "dlgOk");
 		limpar();
 		} catch(Exception e) {
-			DialogMB.showMessage("Falha", "Usuário não pôde ser adicionado.");
+			DialogMB.showMessage("Falha", "Usuário não pôde ser adicionado.", "dlgOk");
 		}
-		
-	}
-
-	public Pessoa buscar() {
-		return dao.buscar(usuario);
-	}
-
-	public void editar() {
-		dao.editar(usuario);
-	}
-
-	public void excluir() {
-		dao.remover(usuario);
 	}
 	
-	public void limpar() {
+	private void limpar() {
 		usuario = new Usuario();
 		dataNasc = null;
 	}
