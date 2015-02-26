2be846c91ae5c9cd6bc3a8d7f6de2f045eebe2b6
Patrick Lima <patrickm.lima1@gmail.com>
2015-02-26 02:20:06 -0400

    essa parte do front end ainda não finalizado

---
+++ TimesLife/src/servico/VerificarListaMonitoradosMB.java
@@ -0,0 +1,11 @@
+package servico;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class VerificarListaMonitoradosMB {
+	private ServicoDAO dao = new ServicoDAO(); 
+
+}
